e4968d7ee26a7df3ecdc994b0a48d3666ef9b94a
Samuel <samuelsantos.sjdr@gmail.com>
2020-07-16 09:49:25 -0300

    Update settings.py
    FIX issue SecretKey

--- sgsa/settings.py
+++ sgsa/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ev$^%ld&jrm=079k7*ui1m_iex(5al0r*d0+*(387#$9xdjqan'
+SECRET_KEY = os.getenv('SGSA_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
